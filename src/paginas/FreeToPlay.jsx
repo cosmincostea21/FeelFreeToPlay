8caaab5b9f61017cf1e52ec706ea0da796fbb0b7
Marius Cosmin Costea <cosmincostea0810@gmail.com>
2025-02-25 10:24:12 +0100

    FooterFreeToPlay

--- src/paginas/FreeToPlay.jsx
+++ src/paginas/FreeToPlay.jsx
@@ -1,6 +1,6 @@
 import Header from '../components/Header.jsx'
 import Navegacion from '../components/Navegacion.jsx'
-import Footer from '../components/footer.jsx'
+import Footer from '../components/Footer.jsx'
 import FreeToPlayMain from '../components/FreeToPlayMain.jsx';
 
 // Layout Free To Play
