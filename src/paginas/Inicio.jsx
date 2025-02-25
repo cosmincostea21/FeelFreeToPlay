b1735c0d04d094615ea719e33a170290ab801ef9
Marius Cosmin Costea <cosmincostea0810@gmail.com>
2025-02-25 10:22:44 +0100

    Footer

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,7 +1,7 @@
 import Main from '../components/Main.jsx'
 import Header from '../components/Header.jsx'
 import Navegacion from '../components/Navegacion.jsx'
-import Footer from '../components/footer.jsx'
+import Footer from '../components/Footer.jsx'
 
 // Layout Inicio
 function Inicio() {
